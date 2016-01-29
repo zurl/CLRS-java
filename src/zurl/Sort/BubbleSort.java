6fa6c0a67d4e1160a2ee7f2ccd21bd1263aaff06
Zhang Chengyi <hellofurry@163.com>
2016-01-29 18:46:12 +0800

    Add  BubbleSort

--- src/zurl/Sort/BubbleSort.java
+++ src/zurl/Sort/BubbleSort.java
@@ -5,7 +5,18 @@ package zurl.Sort;
  */
 public class BubbleSort {
     public static void sort(int[] a){
-
+        for(int i=0;i<=a.length-1;i++){
+            int flag=0;
+            for(int j=0;j<=a.length-2-i;j++){
+                if(a[j]>a[j+1]){
+                    flag=1;
+                    int t=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=t;
+                }
+            }
+            if(flag == 0)break;
+        }
     }
     public static void main(String[] args){
         int[] test = {9,8,7,6,5,4,3};
