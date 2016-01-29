3e7c7b97e4de963f7a18ae001a2a73042ca46d2b
Zhang Chengyi <hellofurry@163.com>
2016-01-29 17:26:12 +0800

    Add  BinarySearch

--- src/zurl/Search/BinarySearch.java
+++ src/zurl/Search/BinarySearch.java
@@ -4,4 +4,23 @@ package zurl.Search;
  * Created by furry on 1/29/2016.
  */
 public class BinarySearch {
+    public static int search(int[] a,int x){
+        //Need Asc Sorted Seq
+        int l=0,r=a.length-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(a[mid]==x){
+                return mid;
+            }else if(a[mid]>x){
+                r=mid-1;
+            }else if(a[mid]<x){
+                l=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] test = {9,8,7,6,5,4,3};
+        System.out.println(search(test,7));
+    }
 }
