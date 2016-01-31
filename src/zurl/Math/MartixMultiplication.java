4e2dabfb6729e2108570f6aa539cda6b90f3caca
Zhang Chengyi <hellofurry@163.com>
2016-01-31 16:44:33 +0800

    Add MartixMultiply

---
+++ src/zurl/Math/MartixMultiplication.java
@@ -0,0 +1,51 @@
+package zurl.Math;
+
+/**
+ * Created by furry on 1/31/2016.
+ */
+public class MartixMultiplication {
+    public static Matrix strassenMultiply(Matrix a,Matrix b) {
+        if(!a.isSquare() || !b.isSquare())
+            throw new RuntimeException("Matrix :: Need Square Martix");
+        if(a.getColumn()!=b.getRow())
+            throw new RuntimeException("Matrix :: Cannot be multiplied");
+        Matrix ret = new Matrix(a.getRow(),b.getColumn());
+        //TODO::
+        return ret;
+    }
+
+    public static Matrix divideAndConquerMultiply(Matrix a,Matrix b) {
+        //need Square
+        if(!a.isSquare() || !b.isSquare())
+            throw new RuntimeException("Matrix :: Need Square Martix");
+        if(a.getColumn()!=b.getRow())
+            throw new RuntimeException("Matrix :: Cannot be multiplied");
+        Matrix ret = new Matrix(a.getRow(),b.getColumn());
+        //TODO::
+        return ret;
+    }
+
+    public static Matrix normalMultiply(Matrix a,Matrix b) {
+        if(a.getColumn()!=b.getRow())
+            throw new RuntimeException("Matrix :: Cannot be multiplied");
+        Matrix ret = new Matrix(a.getRow(),b.getColumn());
+        for(int i=0;i<=a.getRow()-1;i++){
+            for(int j=0;j<=b.getColumn()-1;j++){
+                ret.val[i][j] = 0;
+                for(int k=0;k<=a.getColumn()-1;k++) {
+                    ret.val[i][j] += a.val[i][k]+b.val[k][j];
+                }
+            }
+        }
+        return ret;
+    }
+    public static void main(String[] args){
+        Matrix a = new Matrix(10);
+        Matrix b = new Matrix(10);
+        a.fill(1);b.fill(2);
+        System.out.println("== == == ==");
+        normalMultiply(a,b).print();
+        divideAndConquerMultiply(a,b).print();
+        strassenMultiply(a,b).print();
+    }
+}
---
+++ src/zurl/Math/Matrix.java
@@ -0,0 +1,52 @@
+package zurl.Math;
+
+/**
+ * Created by furry on 1/31/2016.
+ */
+public class Matrix {
+    public int[][] val;
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    int column,row;
+    public Matrix(int _n){
+        val = new int[_n][_n];
+        row=_n;column=_n;
+    }
+    public Matrix(int _n,int _m){
+        val = new int[_n][_m];
+        row=_n;column=_m;
+    }
+    public void fill(int a){
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=column-1;j++) {
+                val[i][j] = a;
+            }
+        }
+    }
+    public void print(){
+        for(int i=0;i<=row-1;i++){
+            for(int j=0;j<=column-1;j++){
+                System.out.print(val[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public boolean isSquare(){
+        return column == row;
+    }
+}
